01bff7377cae9ec982d0769373978f3448d4d6dc
diegaco <diegoacosta211@gmail.com>
2019-03-06 22:58:44 -0300

    lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ message: 'Item API' })
+}
+
 const router = Router()
+
+// This route will be mounted at api/item by the root router
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// api/item/:id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
